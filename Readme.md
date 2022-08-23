af6db01b90b5666a8b4bf68251524de4bcf9de7b
Huyeng <huyeng@iib.tu-darmstadt.de>
2022-08-23 23:39:18 +0200

    :tada: initial commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Geoportal OpenAPI Finder
+
+This repo contains a python script which checks a geoportal for OpenAPI files. If a OpenAPI is found, it will add it to a mapping file and saves the specification into the respected folder.
+
+## Geoportal RLP & Hessen
+
---
+++ find_all_openapis.py
@@ -0,0 +1,73 @@
+import requests
+import json
+import sys
+import time
+from os.path import exists
+
+main_url = ""
+
+
+def call_api(i):
+    url = main_url + str(i) + "/api"
+    response = requests.get(url)
+    return response.text
+
+
+def check_api(i):
+    text = call_api(i)
+    if text.startswith("{") and text.endswith("}"):
+        return True, text
+    else:
+        return False, ""
+
+
+def call_api_all(folder, mappingfile):
+    if not exists(mappingfile):
+        print("no mapping file found")
+        # create mapping file
+        with open(mappingfile, "w") as f:
+            f.write("[]")
+    # read out mapping file
+    mapping = []
+    with open(mappingfile, "r") as f:
+        mapping = json.load(f)
+        for i in range(1, 1000):
+            api, text = check_api(i)
+            if api:
+                # convert text to json and save title and i to textfile
+                repoData = json.loads(text)
+                title = repoData["info"]["title"]
+                print(title)
+                # check if api is already in mapping
+                if (next((item for item in mapping if item["id"] == i), None)) is None:
+                    entry = {
+                        "name": repoData["info"]["title"],
+                        "rawOpenAPI": main_url + str(i) + "/api",
+                        "url": folder + "/" + str(i) + ".json",
+                        "id": i,
+                    }
+                    mapping.append(entry)
+                if exists(folder + "/" + str(i) + ".json"):
+                    print("OpenAPI exists already.")
+                else:
+                    with open(folder + "/" + str(i) + ".json", "w") as myfile:
+                        myfile.write(text)
+                time.sleep(0.1)
+            else:
+                print(i, "not found")
+    with open(mappingfile, "w") as f:
+        json.dump(
+            mapping,
+            f,
+            indent=4,
+        )
+
+
+if __name__ == "__main__":
+    main_url = "https://www.geoportal.rlp.de/spatial-objects/"
+    call_api_all("geoportal-rlp", "geoportal_rlp.json")
+
+    main_url = "https://www.geoportal.hessen.de/spatial-objects/"
+    call_api_all("geoportal-he", "geoportal_he.json")
+    print("done")
+    sys.exit(0)
